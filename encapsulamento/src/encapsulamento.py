07cf71db331138df0b0e5cd13679e4c001d4c00a
Ângelo Hank <hankangelo6@gmail.com>
2026-03-31 19:41:40 -0300

    usando property com fget e fset

--- encapsulamento/src/encapsulamento.py
+++ encapsulamento/src/encapsulamento.py
@@ -1,11 +1,25 @@
 class Televisao():
     def __init__(self, nr_canais):
+        self.__ano_lancamento = None
         self.nr_canais = nr_canais
+        self.__marca = ""
+
 
         # dois underscores torna o atributo privado
         # porem, nao impede de chamar direto com o nome igual o declarado
         # Python evita acesso acidental, não impede acesso intencional
         self.__ligada = False
+
+    # __ano_lancamento = 2000
+    @property # o metodo tem que ter o mesmo nome do atributo
+    # o decorator basicamente transforma esse metodo, em um atributo, pode ser chamado sem parenteses
+    def marca(self):
+        return self.__marca
+
+    @marca.setter
+    def marca(self, marca):
+        self.__marca = marca
+
 
     def aumentar_canais(self, qt_canais):
         self.nr_canais += qt_canais
@@ -20,11 +34,27 @@ class Televisao():
     def __metodo_privado(self):
         print('Metodo Privado')
 
+    def getAno(self):
+        return self.__ano_lancamento
+
+    def setAno(self, ano_lancamento):
+        self.__ano_lancamento = ano_lancamento
+
+    # usando property para definir getter e setter ja na declaracao
+    ano_lancamento = property(fget=getAno, fset=setAno)
+
 tv = Televisao(3)
-print(tv._Televisao__ligada)
+# print(tv._Televisao__ligada)
 
 
 # acessando o atributo privado
 # isso nao deve ser feito, mas eh possivel
-tv._Televisao__ligada = True
-print(tv._Televisao__ligada)
+# tv._Televisao__ligada = True
+# print(tv._Televisao__ligada)
+
+tv.marca = "lalala"
+print(tv.marca)
+
+
+
+print(tv.getAno())
